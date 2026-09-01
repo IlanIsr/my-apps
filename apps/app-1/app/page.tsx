fac6495a7e0e592a6be01886d9c058096450cc6f
Ilan Israel Bellaiche <ilanbellaichepro@gmail.com>
2026-09-01 15:09:06 +0300

    Test app 1 deployment trigger

--- apps/app-1/app/page.tsx
+++ apps/app-1/app/page.tsx
@@ -1,5 +1,10 @@
 import { getHelloMessage } from "@repo/utils";
 
 export default function Page() {
-  return <h1>{getHelloMessage("app 1")}</h1>;
+  return (
+    <>
+      <h1>{getHelloMessage("app 1")}</h1>
+      <p>Only app 1 changed</p>
+    </>
+  );
 }
